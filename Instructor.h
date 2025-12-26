ff8df75361c8168e9859ee454e672fca4603d183
hamzak2232 <149966067+hamzak2232@users.noreply.github.com>
2025-12-27 00:03:30 +0500

    Delete Instructor.h

--- Instructor.h
+++
@@ -1,9 +0,0 @@
-#ifndef INSTRUCTOR_H
-#define INSTRUCTOR_H
-#include "User.h"
-class Instructor : public User {
-public:
-    Instructor(string, string, string);
-    void dashboard() override;
-};
-#endif
