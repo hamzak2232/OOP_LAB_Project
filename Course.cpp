f1b2fb0ece63489a487fd4b28a78893a4a7460a2
hamzak2232 <149966067+hamzak2232@users.noreply.github.com>
2025-12-27 00:02:01 +0500

    Delete Course.cpp

--- Course.cpp
+++
@@ -1,5 +0,0 @@
-#include "Course.h"
-Course::Course(string c, string t, string i)
-: code(c), title(t), instructorID(i) {}
-string Course::getCode() { return code; }
-string Course::getTitle() { return title; }
