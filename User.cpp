66aba40d8323f52f6425063b3a1137c6743e7186
hamzak2232 <149966067+hamzak2232@users.noreply.github.com>
2025-12-27 00:04:41 +0500

    Delete User.cpp

--- User.cpp
+++
@@ -1,6 +0,0 @@
-#include "User.h"
-User::User(string i, string n, string p, string r)
-: id(i), name(n), password(p), role(r) {}
-string User::getID() { return id; }
-string User::getRole() { return role; }
-bool User::authenticate(string p) { return password == p; }
